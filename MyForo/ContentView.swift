582004a72da8547771c10f6152c0f86355c4ef39
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-06-16 20:50:20 -0600

    Initial Commit

---
+++ MyForo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyForo
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 16/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyForo/MyForoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyForoApp.swift
+//  MyForo
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 16/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MyForoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
